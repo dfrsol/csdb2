7b3fbd5bbec0971494bd93ebec7c0a76afd97085
Josh Buselt <dfrsol@gmail.com>
2016-04-28 22:55:30 -0500

    Added node-bourbon and node-neat as npm dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
+var neat = require('node-neat').includePaths;
+var bourbon = require('node-bourbon').includePaths;
+
+const paths = {
+    scss: './client/sass/*.scss'
+};
+
 
 gulp.task('styles', function() {
-    gulp.src('./client/sass/style.scss')
+    gulp.src(paths.scss)
+        .pipe(sass({
+            includePaths: bourbon,
+            includePaths: neat
+        }))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./client/css'));
 });
